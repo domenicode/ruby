5ed0419d5b283b89ab3c3aa656d596b90f56c436
Domenico Vigliotti <domenicovigliotti@Christopher-Vigliottis-MacBook-Pro.local>
2015-11-21 18:22:41 -0500

    page 28

--- kings_string.rb
+++ kings_string.rb
@@ -1,4 +1,5 @@
 kings_string = "Property of His Royal Higness, the King"
+# .reverse is a method or func. that works w/ strings
 string_reflection = kings_string.reverse
 times_to_echo = 3 
 string_echo = kings_string * times_to_echo
